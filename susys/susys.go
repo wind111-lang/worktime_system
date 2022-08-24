abe74e481608bd5f928f5c6b0c710c0a0d4cb817
wind111-lang <k20076kk@aitech.ac.jp>
2022-08-24 21:02:45 +0900

    Add:NewWindow func

---
+++ susys/susys.go
@@ -0,0 +1,27 @@
+package susys
+
+import (
+	"github.com/therecipe/qt/widgets"
+	_"github.com/therecipe/qt/core"
+)
+
+func NewWindow(btn *widgets.QPushButton) {
+	window := widgets.NewQMainWindow(nil, 0)
+	window.SetMinimumSize2(500, 500)
+	window.SetWindowTitle("New window test")
+	//window.SetAttribute(core.Qt__WA_DeleteOnClose, true)
+
+	widget := widgets.NewQWidget(nil, 0)
+	widget.SetLayout(widgets.NewQVBoxLayout())
+	window.SetCentralWidget(widget)
+
+	button := widgets.NewQPushButton2("Close", nil)
+	widget.Layout().AddWidget(button)
+
+	button.ConnectClicked(func(bool) {
+		btn.SetEnabled(true)
+		window.Close()
+	})
+
+	window.Show()
+}
